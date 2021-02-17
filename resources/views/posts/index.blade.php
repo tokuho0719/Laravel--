a9b30a7ff797346da541940a11831f0987a09b51
EC2 Default User <ec2-user@ip-172-31-90-28.ec2.internal>
2021-02-17 13:22:31 +0000

    Laravel基礎4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,17 +2,19 @@
 
 @section('content')
 <div class="container mt-4">
+    @foreach($posts as $post)
     <div class="card mt-4">
         <div class="card-header mb-2">
-                Title
+            {{ $post->title}}
         </div>
         <div class="card-body"> 
-            <p class="card-text">body</p>
+            <p class="card-text">{{ $post->body}}</p>
         </div>
         <div class="card-footer">
             <span class="mr-2">投稿日時</span>
         </div>
     </div>
+    @endforeach
 </div>
 
 @endsection('content')
